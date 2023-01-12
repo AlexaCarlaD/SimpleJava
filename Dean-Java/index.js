386eee7e86d642e55f231d56f5ef6fa03cbee1cb
AlexaCarlaD <121774003+AlexaCarlaD@users.noreply.github.com>
2023-01-12 16:11:40 -0700

    Add files via upload

---
+++ Dean-Java/index.js
@@ -0,0 +1,22 @@
+<html>
+   
+      <body>
+<script>
+         function Simple(firstname, height, country){ 
+
+         console.log('Greetings '+ firstname + '' + height + '' + country)
+         
+         }simple('Carla', '5 foot 3 inches', 'USA');
+
+      </script>
+
+   
+      <button onclick="Print()">Print</button>
+
+      <script>
+         function Print() {alert("I am Carla. My height is 5foot 3inches. I am from the USA")};
+         
+      </script>
+
+   </body>
+</html>
